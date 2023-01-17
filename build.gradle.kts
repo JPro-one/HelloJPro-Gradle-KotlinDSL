aa93d1f7b5083e3578756942e78b6238a53c94e5
Besmir Beqiri <besmir.beqiri@logicbind.io>
2023-01-17 19:49:31 +0100

    Switch to Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    `jpro-gradle-plugin`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+}
+
+javafx {
+    version = "17.0.2"
+    modules = listOf("javafx.base", "javafx.graphics", "javafx.controls", "javafx.fxml", "javafx.media", "javafx.web")
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("one.jpro.hellojpro.HelloJProFXML")
+    mainModule.set(moduleName)
+}
+
+jpro {
+    port = 8080
+}
